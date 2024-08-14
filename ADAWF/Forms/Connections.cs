fa1948519ff88f39c0a10c43389fd8997188895b
Alberto Chirinos <developernet@outlook.com>
2024-08-13 22:30:13 -0500

    Agregar archivos de proyecto.

---
+++ ADAWF/Forms/Connections.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADAWF.Forms
+{
+    public partial class Connections : Form
+    {
+        public Connections()
+        {
+            InitializeComponent();
+        }
+    }
+}
